13369920d5be4af485df223251cfab915f24d5f5
Suhlani Juwan <jhubeishan@gmail.com>
2022-12-19 15:48:38 +0700

    Use cors

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
@@ -8,11 +7,11 @@ const indexRouter = require('./src/v1/routes/index');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/v1', indexRouter);
 
